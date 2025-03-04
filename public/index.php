2b1f9954048110ffc01804d136fe29c14125d8ca
toonette88 <156116373+toonette88@users.noreply.github.com>
2025-03-04 20:39:23 +0100

    essai de deploiement

--- public/index.php
+++ public/index.php
@@ -4,6 +4,12 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+Request::setTrustedProxies(
+    // trust *all* requests
+    ['127.0.0.1', $request->server->get('REMOTE_ADDR')],
+    Request::HEADER_X_FORWARDED_ALL
+);
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
